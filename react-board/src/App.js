fbfc9f2d8ba49cdd23a480aea8e160e538980b0f
JiSeungha <jish0210@naver.com>
2021-05-21 08:49:26 +0900

    Simple board complete

--- react-board/src/App.js
+++ react-board/src/App.js
@@ -1,39 +1,49 @@
+import {useState} from 'react';
 import './App.css';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 function App() {
+  const [content, setContent] = useState({
+    title: '',
+    content: ''
+  })
+
+  const [viewContent, setViewContent] = useState([]);
+
+  const getValue = e => {
+      const {name, value} = e.target;
+      setContent({
+        ...content,
+        [name]: value
+      })
+      console.log(content);
+  };
+
   return (
     <div className="App">
       <h1>BOARD</h1>
       <div className='board'>
-        <h2>Title</h2>
-        <div>
-          Contents
+        {viewContent.map(element =>
+          <div>
+          <h2>{element.title}</h2>
+          <div>
+            {element.content}
+          </div>
         </div>
+        )}
       </div>
       <div className='form-wrapper'>
-              <input className="title-input" type='text' placeholder='Title' />
-              <CKEditor
-                editor={ClassicEditor}
-                data="<p>Insert Contents</p>"
-                onReady={editor => {
-                  // You can store the "editor" and use when it is needed.
-                  console.log('Editor is ready to use!', editor);
-                }}
-                onChange={(event, editor) => {
-                  const data = editor.getData();
-                  console.log({ event, editor, data });
-                }}
-                onBlur={(event, editor) => {
-                  console.log('Blur.', editor);
-                }}
-                onFocus={(event, editor) => {
-                  console.log('Focus.', editor);
-                }}
-              />
+              <input className="title-input" type='text' placeholder='Title' onChange={getValue} name='title'/>
+
             </div>
-            <button className="submit-button">Submit</button>
+            <button
+            className="submit-button"
+            onClick={() => {
+              setViewContent(viewContent.concat({...content}));
+            }
+            }>Submit</button>
+
           </div>
         );
       }
